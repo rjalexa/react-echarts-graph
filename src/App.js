f540bf2f399030e016661c4f2f8350ae2671a830
Robert Alexander <gogonegro@gmail.com>
2024-09-11 11:23:48 +0200

    changed h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EChartsGraph from './components/EChartsGraph';
 function App() {
   return (
     <div className="App">
-      <h1>My ECharts Graph</h1>
+      <h1>Demo Apache ECharts Graph</h1>
       <EChartsGraph />
     </div>
   );
